6f92e00c0ee6863756bc386e9e57d7794fe2e37d
nao saito <naoki.52abc@icloud.com>
2021-01-17 13:39:09 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user,only: [:create, :show, :edit, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   
   def index
-    @tasks = Task.all.order(id: :desc).page(params[:page]).per(3)
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    # @tasks = Task.all.order(id: :desc).page(params[:page]).per(3)
   end
   
   def show
@@ -15,7 +17,9 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    # @micropost = current_user.microposts.build(micropost_params)
+    # @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -53,6 +57,13 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content, :status, :user)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
