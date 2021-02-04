2145fd3254d634cbc78888551f8e7077fd66becb
nao saito <naoki.52abc@icloud.com>
2021-02-04 12:34:31 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user,only: [:create, :show, :edit, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -51,10 +49,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
