f8e53bf31b78209cacc19707d44b86d32d2db3fa
nao saito <naoki.52abc@icloud.com>
2021-02-11 08:22:46 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user,only: [:create, :show, :edit, :destroy]
+  before_action :correct_user,only: [:destroy, :show, :edit]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
